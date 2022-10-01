17b19939c16ca0f82facba348ed320e08ad3f1d9
[Jessica Morse] <[jessicabrookemorse@gmail.com]>
2022-10-01 09:49:34 -0400

    fixed path issues

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,11 +4,11 @@ const path = require("path");
 //route to notes.html 
 module.exports = (app) => {
     app.get("/notes", function (req, res) {
-        res.sendFile(path.join(__dirname, "./public/notes.html"));
+        res.sendFile(path.join(__dirname, "../public", "notes.html"));
     });
 
 // route to index.html
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "./public/index.html"));
+        res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 };
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const dbJson = require("./db/db.json")
+const dbJson = require("./db/db.json");
 // const dbJson = require("..db/db.json")
 
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +12,6 @@ app.use(express.static("public"));
 // require routes files
 require("./routes/api-routes")(app);
 require("./routes/html-routes")(app);
-app.listen(PORT, () => { 
+app.listen(PORT, () => {
   console.log(`App listening on: ${PORT}`);
 });
